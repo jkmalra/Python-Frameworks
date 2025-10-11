8fa6c299ed559f70d83346ddcb33b9caf0573431
Jaskaran Singh <jaskaranmain@gmail.com>
2025-10-11 16:43:40 +0530

    Practice Code: INHERITANCE

--- Python-Code/OOPs.py
+++ Python-Code/OOPs.py
@@ -5,22 +5,43 @@
 #         self.name = name
 #         self.model = model
 #
+#     def full_name(self):
+#         return f"Full Name of Car: {self.brand} {self.name} {self.model}"
+#
+#
 # mycar = car('maruti', 'Zen',1992)
 #
 # print(mycar.brand, mycar.name, mycar.model)
-# ----------------
+# print(mycar.full_name())
+# # ----------------
 
-class Stationary:
-    def __init__(self, Brand, Name, Color):
-        if not isinstance(Brand, str):
-            raise TypeError('Brand must be a string')
-        if not isinstance(Name, str):
-            raise TypeError('Name must be a string')
-        if not isinstance(Color, str):
-            raise TypeError('Color must be a Black or Blue')
-        self.Brand = Brand
-        self.Name = Name
-        self.Color = Color
+# class Stationary:
+#     def __init__(self, Brand, Name, Color):
+#         if not isinstance(Brand, str):
+#             raise TypeError('Brand must be a string')
+#         if not isinstance(Name, str):
+#             raise TypeError('Name must be a string')
+#         if not isinstance(Color, str):
+#             raise TypeError('Color must be a Black or Blue')
+#         self.Brand = Brand
+#         self.Name = Name
+#         self.Color = Color
+#
+# mypen = Stationary('Renolds', 'Trimax', 123)
+# print(mypen.Brand, mypen.Name, mypen.Color)
+# ------------------------
 
-mypen = Stationary('Renolds', 'Trimax', 123)
-print(mypen.Brand, mypen.Name, mypen.Color)
+# INHERITANCE
+class Car:
+    def __init__(self, brand, model):
+        self.Brand = brand
+        self.Model = model
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, batterysize):
+        super().__init__(brand, model)
+        self.BatterSize = batterysize
+
+Tesla = ElectricCar('tesla', 2025, '500 Volt')
+
+print(Tesla.Model, Tesla.Brand, Tesla.BatterSize)
