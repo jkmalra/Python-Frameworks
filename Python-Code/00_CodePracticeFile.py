96bb7403d170800c00b8fbf044157dd0fd038946
Jaskaran Singh <jaskaranmain@gmail.com>
2025-10-31 19:37:11 +0530

    Practice Code

---
+++ Python-Code/00_CodePracticeFile.py
@@ -0,0 +1,17 @@
+# # # print("hello\nworld")
+# # class Car:
+# #     def __init__(self, brand: str, model: int):
+# #         self.brand =  brand
+# #         self.model = model
+# #
+# # car1 = Car('BMW', 2024)
+# # car2 = Car('Volvo', 2023)
+# # car3 = Car('Mercedes', 2025)
+# #
+# # print(type(car1.brand),car1.model)
+#
+# name = str(input('whats ur name?: '))
+# print(name)
+# print(type(name))
+
+
